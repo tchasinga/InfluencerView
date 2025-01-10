3e196f81add321ad4c9b5bf5d446d1d604920f85
Tchasinga jacques <tchasingajacques@gmail.com>
2025-01-10 13:47:14 +0300

    Refactor aply.route.js to rename controller functions for clarity

--- backend/routes/aply.route.js
+++ backend/routes/aply.route.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { createAply, getAply, getAplyById , singout } from "../controllers/aply.controller.js";
+import { createSharing, getSharing, getSharingById } from "../controllers/aply.controller.js";
 
 const router = express.Router(); // Move this line up to initialize the router first
 
-router.post("/create", createAply);
-router.get("/get", getAply);
+router.post("/create", createSharing);
+router.get("/get", getSharing);
 router.get("/getbyid/:id", getAplyById);
-router.get("/signout", singout);
+
 
 export default router;
