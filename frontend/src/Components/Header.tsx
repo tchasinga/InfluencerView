3cf967f33df008271ee49d88a28c5a45313dedf0
Tchasinga jacques <tchasingajacques@gmail.com>
2025-01-10 15:54:02 +0300

    Add tooltip to profile image in Header component for improved accessibility

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { Tooltip } from '@mui/material';
 
 export default function Header() {
 
@@ -10,7 +11,7 @@ export default function Header() {
   );
 
   return (
-    <div className='max-w-screen-xl mx-auto px-4 sm:px-6 flex items-center justify-between py-4 border-b-2 border-gray-100 shadow-sm'>
+    <div className='max-w-screen-xl mx-auto px-4 sm:px-6 flex items-center justify-between py-4 '>
       <div className="text-2xl font-bold">
         Influencer <span className="text-blue-500">View</span>
       </div>
@@ -19,9 +20,11 @@ export default function Header() {
        <div className="flex items-center cursor-pointer">
         {
           currentUser ?
+          <Tooltip title="Profile" placement='bottom' arrow>
             <Link to="/profile">
-                <img src={currentUser.user.userprofile} alt="Profile" className="w-10 h-10 rounded-full" />
+                <img src={currentUser.user?.userprofile} alt="Profile" className="w-10 h-10 rounded-full" />
             </Link>
+          </Tooltip>
             :
             <div>
               <Link to="/signin">
