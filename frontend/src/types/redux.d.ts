66bec2fd7153d21fdfaf46ddcd1cb4a908a59577
Tchasinga jacques <tchasingajacques@gmail.com>
2025-01-10 14:31:19 +0300

    Add react-router-dom dependency and update type declaration for Redux store

--- frontend/src/types/redux.d.ts
+++ frontend/src/types/redux.d.ts
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module '../redux/store.ts' {
-    const store: unknown; // Replace `any` with the correct type of your store
+    const store: any; // Replace `any` with the correct type of your store
     export default store;
   }
   
