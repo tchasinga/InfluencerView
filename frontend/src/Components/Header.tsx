228b8246036969c66439e6f7fd4556c39fb1058d
Tchasinga jacques <tchasingajacques@gmail.com>
2025-01-10 21:41:28 +0300

    Remove unnecessary React imports from Dashboard and Header components

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React from 'react'
+
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Tooltip } from '@mui/material';
