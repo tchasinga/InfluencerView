69dda010cd1037cfb49f3abaea7d996a91f9578b
Tchasinga jacques <tchasingajacques@gmail.com>
2025-01-10 14:22:57 +0300

    Refactor import statements for consistency in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { persistor, store } from "./redux/store.js";
+import { persistor, store } from './redux/store.js'
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')!).render(
 <Provider store={store}>
