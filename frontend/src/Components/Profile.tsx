a54777f30e5fa43d6821d27c6cbca33b6039e768
Tchasinga jacques <tchasingajacques@gmail.com>
2025-01-10 16:00:29 +0300

    Enhance Profile component to display user information and profile image using Redux state

--- frontend/src/Components/Profile.tsx
+++ frontend/src/Components/Profile.tsx
@@ -1,8 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 export default function Profile() {
+  const currentUser = useSelector(
+    (state: any) => state.user && state.user.user.currentUser
+  );
+
   return (
-    <div>
+    <div className='max-w-screen-xl mx-auto px-4 sm:px-6 flex flex-col justify-start'>
+      <div className="">
+          <img src={currentUser.user?.userprofile} alt="Profile" className="w-28 h-28 rounded-full" />
+      </div>
+      <div className="flex flex-col justify-center">
+        <div className="text-2xl font-bold">
+          {currentUser.user?.username}
+        </div>
+        <div className="text-xl text-slate-600 font-normal">
+          {currentUser.user?.email}
+        </div>
+      </div>
     </div>
   )
 }
