bf52e06ba1e22b1d56a6b24253c41de797f1cc0e
Tchasinga jacques <tchasingajacques@gmail.com>
2025-01-10 14:47:42 +0300

    Add SignIn component and user slice for authentication; update dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
+import SignIn from './components/SignIn'
+// import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
 
   return (
     <div>
+      <SignIn />
     </div>
   )
 }
